de9bbae6b37182cc92ff5c727121c677da239c57
paul <deepak.paul@sixsprints.com>
2020-03-13 15:44:55 +0530

    bug fixes

--- src/components/form/search-input.js
+++ src/components/form/search-input.js
@@ -1,7 +1,41 @@
 import React from "react";
 import { asField, Form, Text } from "informed";
-import { IconButton, Button, Box } from "@material-ui/core";
+import { IconButton, Button, Box, makeStyles } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles(theme => ({
+  search: {
+    border: "hidden",
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: theme.palette.common.white,
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.6)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto"
+    }
+  },
+
+  inputInput: {
+    fontSize: 18,
+    border: "hidden",
+    "&:focus": {
+      outline: "none"
+    },
+    backgroundColor: "transparent",
+    padding: theme.spacing(2, 1, 2, 2),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "100%"
+    }
+  }
+}));
 
 const SearchInput = asField(({ ...props }) => {
   const {
@@ -26,17 +60,19 @@ const SearchInput = asField(({ ...props }) => {
     onSearchSubmit(val);
     formApi.reset();
   };
+
+  const classes = useStyles();
   return (
     <Form
       getApi={getFormApi}
-      className={containerClassName}
+      className={containerClassName || classes.search}
       onSubmit={handleSubmit}
     >
       <Box display="flex" justifyContent="space-between">
         <Text
           placeholder={placeholder || "Search..."}
           field="search"
-          className={className}
+          className={className || classes.inputInput}
         />
         {icon && (
           <IconButton type="submit" aria-label="search" color="primary">
