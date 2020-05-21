27e17b95fff52cee7a6b1b33d3a79a669e11d2bc
Deepak Paul <deepak.paul@sixsprints.com>
2020-05-21 11:14:09 +0530

    bug fixes

--- src/components/form/text-input.js
+++ src/components/form/text-input.js
@@ -36,15 +36,12 @@ const TextInput = asField(({ fieldState, fieldApi, ...props }) => {
           variant={variant || "outlined"}
           label={label}
           helperText={error}
-          value={value || ''}
+          value={value || ""}
           margin="normal"
           type={type}
           {...rest}
           onChange={e => {
             let val = e.target.value;
-            if (type === "number" && val === "") {
-              val = 0;
-            }
             if (format) {
               val = format(val);
             }
