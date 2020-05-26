7b3832752fe1207a839204557bb6ddafd08c5e14
Deepak Paul <deepak.paul@sixsprints.com>
2020-05-26 21:02:59 +0530

    custom date input added

---
+++ src/components/form/keyboard-date-picker.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { asField } from "informed";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import { FormHelperText } from "@material-ui/core";
+
+const CustomDateInput = asField(({ fieldState, fieldApi, ...props }) => {
+  const {
+    required,
+    onChange,
+    onBlur,
+    containerClassName,
+    onFocus,
+    label,
+    minDate,
+    className,
+    maxLength,
+    format,
+    hideLabel,
+    ...rest
+  } = props;
+
+  const { error, value = null, touched } = fieldState;
+  const { setValue } = fieldApi;
+
+  return (
+    <div className={containerClassName}>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <div>
+          <KeyboardDatePicker
+            className={`MuiFormControl-marginNormal ${className}`}
+            id="date-picker-dialog"
+            {...rest}
+            format="DD/MM/YYYY"
+            label={label}
+            value={value}
+            onChange={e => {
+              setValue(e.valueOf());
+            }}
+          />
+          <FormHelperText>{error}</FormHelperText>
+        </div>
+      </MuiPickersUtilsProvider>
+    </div>
+  );
+});
+
+export default CustomDateInput;
